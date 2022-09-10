e095baf61d241fc5e11f09e72ed6fa6db69982f2
Linh phan <phanphanvjc@gmail.com>
2022-09-10 09:02:48 +0700

    add scss

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 // library
 const path = require('path');
-const express = require('express'); 
+const express = require('express');
 const morgan = require('morgan');
 const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 // HTTP logger
 app.use(morgan('combined'));
