0aed78e502cf3d556a5bbda8ade62d62c8252c74
Linh phan <phanphanvjc@gmail.com>
2022-09-11 10:44:14 +0700

    mvc

--- src/index.js
+++ src/index.js
@@ -3,13 +3,21 @@ const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const handlebars = require('express-handlebars');
+const { query } = require('express');
 const app = express();
 const port = 3000;
 
+const route = require('./routes');
+
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
 // HTTP logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 // template engine
 app.engine('hbs', handlebars.engine({
@@ -18,14 +26,11 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-// route
-app.get('/', (req, res) => {
-    res.render('home');
-})
+// Route init
+route(app);
 
-app.get('/news', (req, res) => {
-    res.render('news');
-})
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
